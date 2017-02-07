bc122e7ba9ec6750536f7c1e34b87026ce3513b1
Roman Alyakritskiy <rawork@yandex.ru>
2017-02-07 23:13:23 +0300

    initial commit

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {partial} from '../../lib/utils';
+
+export const TodoItem = (props) => {
+  const handleToggle = partial(props.handleToggle, props.id)
+  const handleRemove = partial(props.handleRemove, props.id)
+
+  return (
+    <li>
+      <span className="delete-item"><a href="#" onClick={handleRemove}>&times;</a></span>
+      <input type="checkbox" onChange={handleToggle}
+             checked={props.isComplete}/> {props.name}
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  isComplete: React.PropTypes.bool,
+  name: React.PropTypes.string.isRequired,
+  id: React.PropTypes.number.isRequired
+}
